5616441e9d4717df48239f0aa0a1cf8a0481000a
Seif <seif.dorani@gmail.com>
2018-03-07 16:18:21 -0800

    jquery event handlers, click, with alert for testing

--- JS/game.js
+++ JS/game.js
@@ -1,1 +1,5 @@
-alert("works!!");
+$(document).ready(function(){//selecting the document, then running a function called ready, when this is ready - run this funciton
+  $(document).click(function(){
+    alert("you clicked me");
+  });
+});
