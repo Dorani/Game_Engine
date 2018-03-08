3d85fcfa28e3a23e333915f798a8d149e531cb9b
Seif <seif.dorani@gmail.com>
2018-03-07 16:56:47 -0800

    taking the hero div, giv it a css value of top.eventY and left event.X

--- JS/game.js
+++ JS/game.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){//selecting the document, then running a function called ready, when this is ready - run this funciton
   $(document).click(function(event){//handler click function
-    alert("you clicked me " + event.pageX + " " + event.pageY);
+    //alert("you clicked me " + event.pageX + " " + event.pageY);
+    $("#hero_div").css("top", event.pageY + "px");
+    $("#hero_div").css("left", event.pageX + "px");
   });
 });
