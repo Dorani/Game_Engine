0e0db07403538b15ae03f0a2a60fbf61aa27ccda
Seif <seif.dorani@gmail.com>
2018-03-07 16:53:26 -0800

    getting coordinates of location of the click event

--- JS/game.js
+++ JS/game.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){//selecting the document, then running a function called ready, when this is ready - run this funciton
-  $(document).click(function(){
-    alert("you clicked me");
+  $(document).click(function(event){//handler click function
+    alert("you clicked me " + event.pageX + " " + event.pageY);
   });
 });
