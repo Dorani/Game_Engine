34441e52f816889c28b8b91eebdc589bc3469fab
Seif <seif.dorani@gmail.com>
2018-03-07 17:21:45 -0800

    assigned ids to the other divs and created a world open obj, and a people open array, and will then be storing value for easy retrieval, to know where everything is, to see if people are hitting each other

--- JS/game.js
+++ JS/game.js
@@ -1,7 +1,16 @@
+var world = {};//empty obj called world
+
+
+
 $(document).ready(function(){//selecting the document, then running a function called ready, when this is ready - run this funciton
   $(document).click(function(event){//handler click function
     //alert("you clicked me " + event.pageX + " " + event.pageY); when clicked, alert will display coordinates of the event
     $("#hero_div").css("top", event.pageY + "px");
     $("#hero_div").css("left", event.pageX + "px");
   });
+  world.people = [];//collection to iterate through
+  world.people.push({el: $("#extra1"), top: 0, left:0 });
+  //storing value for easy retrieval, to know where everything is, to see if people are hitting each other
+  world.people.push({el: $("#extra2"), top: 0, left:0 });
+
 });
