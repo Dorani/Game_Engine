5f836269b8ef704539650699371510d30f272409
Seif <seif.dorani@gmail.com>
2018-03-07 17:05:47 -0800

    used css transiton properties on the person class, to have it gradually move and show its movements over time rather than teleport from one area to the next

--- JS/game.js
+++ JS/game.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){//selecting the document, then running a function called ready, when this is ready - run this funciton
   $(document).click(function(event){//handler click function
-    //alert("you clicked me " + event.pageX + " " + event.pageY);
+    //alert("you clicked me " + event.pageX + " " + event.pageY); when clicked, alert will display coordinates of the event
     $("#hero_div").css("top", event.pageY + "px");
     $("#hero_div").css("left", event.pageX + "px");
   });
