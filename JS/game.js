4a6bf7921c2668d9bad46a3877fb398d668bd259
Seif <seif.dorani@gmail.com>
2018-03-07 17:48:58 -0800

    randomizes location of x and y coordinates

--- JS/game.js
+++ JS/game.js
@@ -9,8 +9,18 @@ $(document).ready(function(){//selecting the document, then running a function c
     $("#hero_div").css("left", event.pageX + "px");
   });
   world.people = [];//collection to iterate through
-  world.people.push({el: $("#extra1"), top: 0, left:0 });
+  world.people.push({el: $("#extra1"), top: 0, left:0, directionX: 1, directionY: 1 });//0 element of the array, random direction
   //storing value for easy retrieval, to know where everything is, to see if people are hitting each other
-  world.people.push({el: $("#extra2"), top: 0, left:0 });
-
+  world.people.push({el: $("#extra2"), top: 0, left:0, directionX: 1, directionY: 1 });// 1 element of array
 });
+
+function movePeople(){//creating a function called movePeople to randomize movements
+  for (num in world.people){
+    var person = world.people[num];//able to create more people
+    //giving the key, arrays have keys that start at 0
+    person.top = person.top + (parseInt(Math.random() * 10) * person.directionY);
+    //math . random gives a number between 0 and 9 and that we are multiplying by directoion y and x
+    person.left = person.left + (parseInt(Math.random() * 10) * person.directionX);//math . random gives a number between 0 and 9 and that we are multiplying by directoion y and x
+
+  }
+}
